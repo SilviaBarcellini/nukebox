78be7c3ba894987cb0fad735173d767b8b70e9dd
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-02-25 17:05:44 +0100

    🐛 Read newest db on every request

--- pages/api/tracks/index.ts
+++ pages/api/tracks/index.ts
@@ -1,7 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { NextApiRequest, NextApiResponse } from "next";
-import db from "../../../server/db.json";
+//OLD import db from "../../../server/db.json";
+import { readDb } from "../../../server/db";
 
+//OLD export default (_req: NextApiRequest, res: NextApiResponse) => {
 export default (_req: NextApiRequest, res: NextApiResponse) => {
+  const db = await readDb();
   res.status(200).json(db.tracks);
 };
--- utils/api.ts
+++ utils/api.ts
@@ -61,5 +61,5 @@ export async function deleteTrack(id: string) {
     //const deletedTrack = "track successfully removed";
     //return deletedTrack
   );
-  alert("Item removed from your playlist!");
+  alert("Song removed from your playlist!");
 }
